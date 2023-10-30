a7fc5bdb3be3011f38ac97a21c0b97ff3e35fa90
Ing. Gabriel Solano <dgabrielsolo@gmail.com>
2023-10-29 23:19:12 -0600

    .

--- scripts/pages.js
+++ scripts/pages.js
@@ -2,5 +2,5 @@ d = document
 just_name = d.getElementById('just-name')
 
 just_name.addEventListener('click', ()=>{
-    window.location.href = 'index.html'
+    window.location.href = '../index.html'
 })
