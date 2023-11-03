5e7a4f365e82ce12e8b2cfd13bbfbfb6eef91fda
Ing. Gabriel Solano <dgabrielsolo@gmail.com>
2023-11-03 15:54:08 -0600

    .

--- scripts/pages.js
+++ scripts/pages.js
@@ -1,6 +1,24 @@
 d = document
 just_name = d.getElementById('just-name')
+core_area = d.getElementsByClassName('core-area')
+nav_bar_item = d.getElementsByClassName('nav-bar-item')
 
 just_name.addEventListener('click', ()=>{
     window.location.href = '../index.html'
+})
+
+try{
+    core_area[0].addEventListener('click', ()=>{
+        window.location.href = 'en-review.html'
+    })
+}catch(err){
+    console.log(err)
+}
+
+nav_bar_item[0].addEventListener('click', ()=>{
+    window.location.href = 'en-process-autom.html'
+})
+
+nav_bar_item[1].addEventListener('click', ()=>{
+    window.location.href = 'en-home.html'
 })
